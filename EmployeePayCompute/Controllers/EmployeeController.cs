08a42f639a31277d02e84ea3027f3db87d061db9
Abhishek Srivastava <admin@as3globalsolution.com>
2021-02-13 20:28:49 +0530

    Create and Edit -EmployeeController

--- EmployeePayCompute/Controllers/EmployeeController.cs
+++ EmployeePayCompute/Controllers/EmployeeController.cs
@@ -50,7 +50,7 @@ namespace EmployeePayCompute.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken] // Prevent cross site Reuest Forgery Attacks
-        public async Task <IActionResult> Create(EmployeeCreateViewModel model)
+        public async Task<IActionResult> Create(EmployeeCreateViewModel model)
         {
             if (ModelState.IsValid)
             {
@@ -79,17 +79,94 @@ namespace EmployeePayCompute.Controllers
                 if (model.ImageUrl != null && model.ImageUrl.Length > 0)
                 {
                     var uploadDir = @"images/employee";
-                    var fileName = Path.GetFileNameWithoutExtension( model.ImageUrl.FileName);
+                    var fileName = Path.GetFileNameWithoutExtension(model.ImageUrl.FileName);
                     var extension = Path.GetExtension(model.ImageUrl.FileName);
                     var webRootPath = _hostingEnviroment.WebRootPath;
                     fileName = DateTime.UtcNow.ToString("yymmssfff") + fileName + extension;
                     var path = Path.Combine(webRootPath, uploadDir, fileName);
-                   await model.ImageUrl.CopyToAsync( new FileStream(path,FileMode.Create));
-                    employee.ImageUrl = "/" + uploadDir + "/" + fileName; 
+                    await model.ImageUrl.CopyToAsync(new FileStream(path, FileMode.Create));
+                    employee.ImageUrl = "/" + uploadDir + "/" + fileName;
                 }
 
                 await _employeeService.CreateAsync(employee);
-                return RedirectToAction(nameof (Index));
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        [HttpGet]
+
+        public IActionResult Edit(int id)
+        {
+            var employee = _employeeService.GetByID(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            var model = new EmployeeEditViewModel()
+            {
+                ID = employee.ID,
+                EmployeeNo = employee.EmployeeNo,
+                FirstName = employee.FirstName,
+                LastName = employee.LastName,
+                Gender = employee.Gender,
+                Email = employee.Email,
+                DOB = employee.DOB,
+                DateJoined = employee.DateJoined,
+                NationalInsurenceNo = employee.NationalInsurenceNo,
+                PaymentMethod = employee.PaymentMethod,
+                StudentLoan = employee.StudentLoan,
+                UnionMember = employee.UnionMember,
+                Address = employee.Address,
+                City = employee.City,
+                Phone = employee.Phone,
+                PostCode = employee.PostCode,
+                Designation = employee.Designation,
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(EmployeeEditViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var employee = _employeeService.GetByID(model.ID);
+                if (employee == null)
+                {
+                    return NotFound();
+                }
+                employee.EmployeeNo = model.EmployeeNo;
+                employee.FirstName = model.FirstName;
+                employee.LastName = model.LastName;
+                employee.MiddleName = model.MiddleName;
+                employee.NationalInsurenceNo = model.NationalInsurenceNo;
+                employee.Gender = model.Gender;
+                employee.Email = model.Email;
+                employee.DOB = model.DOB;
+                employee.DateJoined = model.DateJoined;
+                employee.Phone = model.Phone;
+                employee.Designation = model.Designation;
+                employee.PaymentMethod = model.PaymentMethod;
+                employee.StudentLoan = model.StudentLoan;
+                employee.UnionMember = model.UnionMember;
+                employee.Address = model.Address;
+                employee.City = model.EmployeeNo;
+                employee.PostCode = model.EmployeeNo;
+                if (model.ImageUrl != null && model.ImageUrl.Length > 0)
+                {
+                    var uploadDir = @"images/employee";
+                    var fileName = Path.GetFileNameWithoutExtension(model.ImageUrl.FileName);
+                    var extension = Path.GetExtension(model.ImageUrl.FileName);
+                    var webRootPath = _hostingEnviroment.WebRootPath;
+                    fileName = DateTime.UtcNow.ToString("yymmssfff") + fileName + extension;
+                    var path = Path.Combine(webRootPath, uploadDir, fileName);
+                    await model.ImageUrl.CopyToAsync(new FileStream(path, FileMode.Create));
+                    employee.ImageUrl = "/" + uploadDir + "/" + fileName;
+                }
+                await _employeeService.UpdateAsync(employee);
+                return RedirectToAction(nameof(Index));
             }
             return View();
         }

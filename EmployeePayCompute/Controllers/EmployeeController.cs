da310aa9469a225114fc891c686c871c8bb9a2a9
Abhishek Srivastava <admin@as3globalsolution.com>
2021-02-13 12:34:11 +0530

    Index - EmployeeController

---
+++ EmployeePayCompute/Controllers/EmployeeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeePayCompute.Models;
+using EmployeePayCompute.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeePayCompute.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeeController(IEmployeeService employeeService)
+        {
+             _employeeService = employeeService;
+
+        }
+
+        public IActionResult Index()
+        {
+            var employee = _employeeService.GetAll().Select(employee=> new EmployeeIndexViewModel
+            {
+                ID= employee.ID,
+                EmployeeNo= employee.EmployeeNo,
+                ImageUrl= employee.ImageUrl,
+                FullName= employee.FullName,
+                Gender= employee.Gender,
+                Designation= employee.Designation,
+                City= employee.City,
+                DateJoined= employee.DateJoined
+            }).ToList();
+            return View();
+        }
+
+    }
+}

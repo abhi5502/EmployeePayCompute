4df51f31971b632eb1175744b95c8983327187a6
Abhishek Srivastava <admin@as3globalsolution.com>
2021-02-24 18:10:25 +0530

    NationalInsuranceContributionService

---
+++ EmployeePayCompute.Services/Implementation/NationalInsuranceContributionService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeePayCompute.Services.Implementation
+{
+    public class NationalInsuranceContributionService : INationalInsuranceContributionService
+    {
+        private decimal NIRate;
+        private decimal NIC;
+        public decimal NIContribution(decimal totalAount)
+        {
+            if (totalAount<719)
+            {
+                // Lower Earning Limit Rate & Below Primary Threshold
+
+                NIRate = .0m;
+                NIC = 0m;
+            }
+            else if(totalAount>=719 && totalAount<=4167)
+            {
+                //Between Primary Threshold and Upper Earnings Limit (UEL)
+
+                NIRate = .12m;
+                NIC = ((totalAount - 719) * NIRate);
+            }
+            else if(totalAount> 4167)
+            {
+                // Above Upper Earnings Limit (UEL)
+                NIRate = .02m;
+                NIC = ((4167 - 719) * .12m) + ((totalAount - 4167) * NIRate);
+            }
+            return NIC;
+        }
+    }
+}
